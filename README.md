0720186c122079683bdfd998bdf75338d6c07bad
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-03 15:53:12 +0200

    Changed to new version of gulp, scss->sass

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # gulp-pro-starter-template
+
 This is the starting template, for those who want to use PUG, SCSS in their own projects.
 
-## How to install it?
+## Usage
+
+### 1. Clone repo
 ```
-cd YourFolderParent(example: C:\Users\Admin\Desktop\projects)
-git clone https://github.com/VsevolodKurochka/gulp-start-template.git
-cd gulp-start-template
+git clone https://github.com/VsevolodKurochka/gulp-pro-starter-template.git
+```
+
+### 2. Go inside cloned repo
+```
+cd gulp-pro-starter-template
+```
+
+### 3. Install all dependencies (make sure nodejs with npm is installed on your machine)
+```
 npm install
 ```
-## Now, you should run the server:
+
+### 4. Run default gulp task (will open browser window with live reload)
 ```
 gulp
 ```
-## You can work!
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,138 +1,61 @@
 'use strict';
 
-// General
+/**
+ * General
+ * -----------------------------------------------------------------------------
+ */
 
 import gulp 						from 'gulp';
-import data 						from 'gulp-data';
-import install 					from 'gulp-install';
-//import browserSync 			from 'browser-sync';
-import notify 					from 'gulp-notify';
-import fs 							from 'fs';
+import folders					from './tasks/folders';
 
+/**
+ * Require modules
+ * -----------------------------------------------------------------------------
+ */
 
-// PUG
+import requireDir from 'require-dir';
 
-import gulpif 					from 'gulp-if';
-import pug 							from 'gulp-pug';
-//import emitty 					from 'emitty';
+requireDir('./tasks');
 
-
-// Sass
-
-import sass 						from 'gulp-sass';
-import sassGlob 				from 'gulp-sass-glob';
-import autoprefixer 		from 'gulp-autoprefixer';
-import csscomb 					from 'gulp-csscomb';
-
-
-// JS
-
-import babel 						from 'gulp-babel';
-import browserify 			from 'browserify';
-import source 					from 'vinyl-source-stream';
-import babelify 				from "babelify";
-import merge 						from 'gulp-merge-json';
-
-
-// Init
-const browserSync = require('browser-sync').create();
-const emitty 			= require('emitty').setup('app', 'pug');
-
-//- Install packages From package.json
-//- Usage: npm install
-gulp.src(['./package.json'])
-	.pipe(install())
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', () =>
-	gulp.src("app/sass/**/*.scss")
-		.pipe(sassGlob())
-		.pipe(sass({
-			outputStyle: 'expanded'
-		}).on('error', sass.logError))
-		
-		.pipe(csscomb())
-		.pipe(autoprefixer({
-			browsers: ['last 15 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest("app/css"))
-		.pipe(browserSync.stream())
-);
-
-// PUG
-
-var locals = {
-	portfolio: JSON.parse(fs.readFileSync('app/data/portfolio.json')),
-	nav: JSON.parse(fs.readFileSync('app/data/nav.json')),
-};
-
-gulp.task('JSON', () =>
-	gulp.src('app/data/**/*.json')
-		.pipe(merge())
-		.on('error', notify.onError({
-				title: 'JSOn Error',
-				message: '<%= error.message %>'
-		}))
-		.pipe(gulp.dest('app/data'))
-);
-
-gulp.task('templates', () =>
-	gulp.src('app/**/*.pug')
-		.pipe(gulpif(global.watch, emitty.stream(global.emittyChangedFile)))
-		.pipe(data( () => {
-			return JSON.parse(fs.readFileSync('app/data/combined.json'));
-		}))
-		.pipe(
-			pug({
-				pretty: true
-			})
-		)
-		.on('error', notify.onError({
-				title: 'PUG Error',
-				message: '<%= error.message %>'
-		}))
-		.pipe(gulp.dest('app'))
-);
-
-// JS
-
-
-gulp.task('babel', () => 
-	gulp.src('app/babel/**/*.js')
-		.pipe(babel())
-		.on('error', notify.onError({
-				title: 'Babel Error',
-				message: '<%= error.message %>'
-		}))
-		.pipe(gulp.dest('app/js'))
-);
-
+import {server, reload, serve} from './tasks/browserSync';
 
 // Your "watch" task
-gulp.task('watch', () => {
+gulp.task(
+	'watch', 
+	gulp.parallel(
+		serve,
+		'sass',
+		'templates',
+		'scripts',
+		'concat',
+		//'webpack',
+		//'bundle-modules',
+		'image',
+		'fonts',
+		'sass:watch',
+		'templates:watch',
+		'scripts:watch',
+		'concat:watch',
+		//'webpack:watch',
+		//'bundle-modules:watch',
+		'image:watch',
+		'fonts:watch'
+	)
+);
 
-	// Shows that run "watch" mode
-	global.watch = true;
-
-	browserSync.init({
-		server: "./app",
-		notify: false
-	});
-
-	gulp.watch('app/**/*.pug', gulp.series('templates'))
-		.on('all', (event, filepath) => {
-			global.emittyChangedFile = filepath;
-		});
-
-	gulp.watch("app/**/*.html").on('all', browserSync.reload);
-	
-	gulp.watch("app/sass/**/*.scss", gulp.series('sass'));
-
-	gulp.watch("app/babel/**/*.js", gulp.series('babel'));
-
-	gulp.watch("app/data/*.json").on('all', gulp.series('JSON'));
-
-});
+// Build
+gulp.task(
+	'build',
+	gulp.series(
+		'clean',
+		gulp.parallel(
+			'sass',
+			'templates',
+			'scripts',
+			'image',
+			'fonts'
+		)
+	)
+);
 
 gulp.task('default', gulp.series('watch'));
