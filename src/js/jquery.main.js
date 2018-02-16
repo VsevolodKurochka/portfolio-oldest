3e09602dded7ffe37d100eeb3df505ec05f440d6
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-16 13:16:39 +0200

    Change testimonial section, add nav to testimonials

--- src/js/jquery.main.js
+++ src/js/jquery.main.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	$('#owl-testimonial').owlCarousel({
 		items: 1,
-		autoplay: true,
-		autoplayTimeout: 3000
+		autoplay: false,
+		autoplayTimeout: 3000,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
 	});
 });	
