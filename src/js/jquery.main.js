7f38c0d79e8d52c417dc6d66ff8f371f57e33321
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-16 15:04:56 +0200

    Change navigation logo color text

--- src/js/jquery.main.js
+++ src/js/jquery.main.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
 		nav: true,
 		navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
 	});
+
+	// $('#portfolio-content').owlCarousel({
+	// 	loop: true,
+	// 	items: 1,
+	// 	autoplay: false,
+	// 	autoplayTimeout: 3000,
+	// 	dots: false,
+	// 	nav: true,
+	// 	navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+	// });
 });	
